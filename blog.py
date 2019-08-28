76abfdf1fb2654d2ef0acc4b96373e12e3e4885c
Anna Górska <ania_los@tlen.pl>
2019-08-28 15:43:11 +0200

    other thigns stil doesnt work

--- blog.py
+++ blog.py
@@ -6,7 +6,7 @@ from flask_frozen import Freezer
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
-FLATPAGES_ROOT = 'content'
+# FLATPAGES_ROOT = 'content'
 POST_DIR = 'posts'
 
 app = Flask(__name__)
@@ -14,23 +14,31 @@ flatpages = FlatPages(app)
 freezer = Freezer(app)
 app.config.from_object(__name__)
 
+# Routes
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-@app.route("/posts/")
+
+@app.route("/projects")
+def home():
+    """ home """
+    return render_template("projects.html")
+
+
+@app.route("/blog/")
 def posts():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
     posts.sort(key=lambda item: item['date'], reverse=False)
-    return render_template('posts.html', posts=posts)
+    return render_template('blog.html', posts=posts)
 
 
-@app.route('/posts/<name>/')
+@app.route('/blog/<name>/')
 def post(name):
     path = '{}/{}'.format(POST_DIR, name)
     post = flatpages.get_or_404(path)
     return render_template('post.html', post=post)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        freezer.freeze()
-    else:
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
